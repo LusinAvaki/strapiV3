56f5dc74b2491ea1306277ec5e8e83e9f03dcb91
LusinAvaki <lusinavaki@gmail.com>
2023-03-16 16:11:20 +0400

    Check user id from token

--- api/meeting/models/meeting.js
+++ api/meeting/models/meeting.js
@@ -5,18 +5,26 @@
  * to customize this model
  */
 
+const axios = require('axios');
+
 module.exports = {
     lifecycles: {
-        afterCreate(data) {
-            if(!data.userId){
-                throw new Error("User not found")
-            } else {
-                strapi.query('notification').create({
-                    userId: data.userId.id,
-                    notificationText: "You created a meeting",
-                    read: false,
-                });
+        async afterCreate(response) {
+            if (!response.userId) {
+                throw new Error("Invalid input data. Please verify unique constraints!");
             }
+            const { data } = await axios.post('http://localhost:1337/auth/local', {
+                identifier: response.userId.firstname,
+                password: 'test1234',
+            });
+            if (!data && !data.user.id) {
+                throw new Error("User not found!")
+            } 
+            strapi.query('notification').create({
+                userId: data.user.id,
+                notificationText: "You created a meeting",
+                read: false,
+            });
         }
     }
 };
